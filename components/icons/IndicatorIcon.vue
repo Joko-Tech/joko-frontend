6ceb621acf741efd0589e3eca2081460719396b0
Oluwadare Seyi <oluwadareseyii@gmail.com>
2022-07-28 18:55:04 +0100

    feat: add button component

---
+++ components/icons/IndicatorIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg
+    viewBox="0 0 10 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle r="5" transform="matrix(-1 0 0 1 5 5.5)" fill="#BF0000" />
+  </svg>
+</template>
---
+++ components/icons/LockIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M19 10H5a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2Z"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 6a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3v4H6V6Z"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
