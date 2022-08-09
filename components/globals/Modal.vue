18e46a0c4c7c44d069930826c6058bde8c98a13a
Oluwadare Seyi <oluwadareseyii@gmail.com>
2022-08-09 15:15:14 +0100

    feat: add base modal component

--- components/globals/Modal.vue
+++ components/globals/Modal.vue
@@ -1,9 +1,59 @@
 <template>
-  <div>Modal</div>
+  <transition name="modal-fade">
+    <div
+      ref="modal"
+      class="c-modal"
+      role="dialog"
+      :aria-labelledby="title"
+      :aria-describedby="description"
+      @keydown.esc="close"
+      tabindex="0"
+    >
+      <div class="c-modal__main">
+        <slot />
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
-export default {};
-</script>
+export default {
+  name: "Modal",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    transition: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    focus() {
+      this.$nextTick(() => {
+        const modalRef = this.$refs.modal;
+        modalRef.focus();
+      });
+    },
+  },
+  watch: {
+    "$store.state.token.isTokenModalOpen": function (newState, oldState) {
+      if (newState) {
+        this.focus();
+      }
+    },
 
-<style lang="scss" scoped></style>
+    $route() {
+      this.close();
+    },
+  },
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="c-layout">
+    <Modal
+      title="token modal"
+      description="a modal showing more details of a specific token"
+      v-show="isTokenModalOpen"
+      @close="hideTokenModal"
+    >
+      Hello Modal
+    </Modal>
     <div class="c-pages">
       <NavComponent />
       <div class="c-main"><Nuxt /></div>
@@ -8,4 +16,20 @@
   </div>
 </template>
 
-<script></script>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters({
+      isTokenModalOpen: "token/isTokenModalOpen",
+    }),
+  },
+
+  methods: {
+    hideTokenModal() {
+      this.$store.commit("token/updateIsTokenModalOpen", false);
+    },
+  },
+};
+</script>
