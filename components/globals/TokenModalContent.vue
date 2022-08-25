d5ce22037be76006f10a8b6387f3d4776e03aa73
Oluwadare Seyi <oluwadareseyii@gmail.com>
2022-08-25 13:23:21 +0100

    feat: finish token modal base layout

--- components/globals/TokenModalContent.vue
+++ components/globals/TokenModalContent.vue
@@ -32,6 +32,42 @@
         <div class="c-content__description">
           {{ token.description }}
         </div>
+
+        <div class="c-content__panel">
+          <div class="c-content__market">
+            <div class="c-content__market__label">Editions</div>
+            <div class="c-content__market__edition">
+              <!-- <span>{{ token.tokenIndex }}/{{ token.tokenCount }}</span> -->
+              <span>1/1</span>
+              <span>
+                <ToolTip>Number of tokens in this edition</ToolTip>
+              </span>
+            </div>
+          </div>
+
+          <div class="c-content__market">
+            <div class="c-content__market__label">Lowest ask</div>
+            <div class="c-content__market__amount">
+              <span>10</span> <span><TezosIcon /></span>
+            </div>
+          </div>
+
+          <div class="c-content__market">
+            <div class="c-content__market__label">Highest bid</div>
+            <div class="c-content__market__amount">
+              <span>10</span> <span><TezosIcon /></span>
+            </div>
+          </div>
+
+          <div class="c-content__market">
+            <div class="c-content__market__label">Last price</div>
+            <div class="c-content__market__amount">
+              <span>10</span> <span><TezosIcon /></span>
+            </div>
+          </div>
+
+          <ButtonComponent size="large" filled>Sell</ButtonComponent>
+        </div>
       </div>
     </div>
   </div>
