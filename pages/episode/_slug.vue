6c0abe6113bacadc02b3581472411d5e4deefc77
Oluwadare Seyi <oluwadareseyii@gmail.com>
2022-11-25 13:31:26 +0100

    patch: delete extra dynamic page

--- pages/episode/_slug.vue
+++ pages/episode/_slug.vue
@@ -3,12 +3,13 @@
     <section>
       <h1 class="c-episodepage__title">EPISODE 1 - CHOICES</h1>
       <div class="c-episodepage__video">
-        <video
+        <!-- <video
           src="https://res.cloudinary.com/dmwfd0zhh/video/upload/v1645290917/Pith%20Africa/PITH-_JFE_compressed_xjx09l.mp4"
           poster="https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1645453390/Pith%20Africa/Screenshot_2022-02-21_at_3.21.43_PM_xx2lrw.jpg"
           preload="auto"
           controls
-        />
+        /> -->
+        <video-player :options="videoOptions" />
       </div>
       <div class="c-episodepage__bio">
         <div class="c-episodepage__bio__label">Bio</div>
@@ -83,6 +84,21 @@
 
 <script>
 export default {
+  data() {
+    return {
+      videoOptions: {
+        controls: true,
+        preload: true,
+        fluid: true,
+        sources: [
+          {
+            src: "https://d2o1rek401wuzo.cloudfront.net/assets/VANLIFE/HLS/JOKO-IMRAN_360.m3u8?Expires=1669356976&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kMm8xcmVrNDAxd3V6by5jbG91ZGZyb250Lm5ldC9hc3NldHMvVkFOTElGRS9ITFMvSk9LTy1JTVJBTl8zNjAubTN1OCIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTY2OTM1Njk3Nn19fV19&Signature=taV-yQWgBLmrJe1Kw0xXTQPOvrmE764TzQbvndwndyvU7YochnZD5SK5fIHsl~MA460na5HC-JBfP~L3L4KVJ9rz7LXbh4tduk0YmkCqsWgGofE6EUpPvQG27LnIhbcGt1LFwZktuFA7nTJhWu4Epib4K5IZk7u01AdNevyTo7jh1S~G8FkBtZIUUwoIzbSXQlJV0rUwzZYwKkJ78SYaDoMUXQQvUXodea16b4TaTbeladBfi9RdKksBXDX45AEtukuZvNeDlpQlQKcjShBoYZcv4FGRHVwfPGqIWjybXn9hZkXFU4F~vfO~OfwJn3HihEDMoYgrkwcbJz0g81LW7w__&Key-Pair-Id=KFDSTL4XUWKY1",
+            type: "application/x-mpegURL",
+          },
+        ],
+      },
+    };
+  },
   computed: {
     slug() {
       return this.$route.params.slug;
