9e0540ba8b0597918e2abdfd390f535d8bc463f5
Mihir018 <mihirkasundra1@gmail.com>
2022-11-25 18:19:47 +0530

    temp video changes

--- pages/episode/_slug.vue
+++ pages/episode/_slug.vue
@@ -83,6 +83,21 @@
 </template>
 
 <script>
+
+import VideoPlayer from '@/components/VideoPlayer.vue';
+  import  { getSignedUrl } from '@aws-sdk/cloudfront-signer';
+  
+  const key = Buffer.from(process.env.PRIVATE_KEY , 'base64').toString('ascii');
+
+  const imageUrl = getSignedUrl({
+    url: "https://d2o1rek401wuzo.cloudfront.net/assets/VANLIFE/HLS/JOKO-IMRAN_360.m3u8",
+    dateLessThan: new Date(Date.now() + 1000 * 60 * 60 *24),
+    privateKey: key,
+    keyPaidId: process.env.CLOUDFRONT_KEYPAIR_ID,
+  })
+
+  console.log(imageUrl);
+
 export default {
   data() {
     return {
@@ -92,7 +107,7 @@ export default {
         fluid: true,
         sources: [
           {
-            src: "https://d2o1rek401wuzo.cloudfront.net/assets/VANLIFE/HLS/JOKO-IMRAN_360.m3u8?Expires=1669356976&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kMm8xcmVrNDAxd3V6by5jbG91ZGZyb250Lm5ldC9hc3NldHMvVkFOTElGRS9ITFMvSk9LTy1JTVJBTl8zNjAubTN1OCIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTY2OTM1Njk3Nn19fV19&Signature=taV-yQWgBLmrJe1Kw0xXTQPOvrmE764TzQbvndwndyvU7YochnZD5SK5fIHsl~MA460na5HC-JBfP~L3L4KVJ9rz7LXbh4tduk0YmkCqsWgGofE6EUpPvQG27LnIhbcGt1LFwZktuFA7nTJhWu4Epib4K5IZk7u01AdNevyTo7jh1S~G8FkBtZIUUwoIzbSXQlJV0rUwzZYwKkJ78SYaDoMUXQQvUXodea16b4TaTbeladBfi9RdKksBXDX45AEtukuZvNeDlpQlQKcjShBoYZcv4FGRHVwfPGqIWjybXn9hZkXFU4F~vfO~OfwJn3HihEDMoYgrkwcbJz0g81LW7w__&Key-Pair-Id=KFDSTL4XUWKY1",
+            src: {imageUrl},
             type: "application/x-mpegURL",
           },
         ],
